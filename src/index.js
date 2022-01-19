8f5ee75527d9351db7168d38ecc243057437eb67
Angel Rosario <arosario.us@gmail.com>
2022-01-19 10:31:47 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log('%c HI', 'color: firebrick')
+// console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+function fetchDog(){
+    fetch(imgUrl)
+    .then(response => response.json())
+    .then(data => {
+        const dogArray = data.message
+        dogArray.forEach(renderImages)
+        // console.log(data.message)
+    })
+}
+fetchDog()
+
+function renderImages(){
+    // take a url and make an image out of it
+    const img = document.createElement('img')
+    img.src = "https://dog.ceo/api/breeds/image/random/4"
+    document.getElementById('dog-breeds').append(img)
+}
