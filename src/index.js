7a4158d0676ec3f41045eb55db1bfbc6ccc71439
April Skrine <april.skrine@gmail.com>
2022-01-19 09:53:59 -0600

    commit 2

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // console.log('%c HI', 'color: firebrick')
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
 
 function fetchDog(){
     fetch(imgUrl)
@@ -7,14 +8,28 @@ function fetchDog(){
     .then(data => {
         const dogArray = data.message
         dogArray.forEach(renderImages)
-        // console.log(data.message)
     })
 }
 fetchDog()
+fetchBreeds()
 
-function renderImages(){
-    // take a url and make an image out of it
+function renderImages(someUrl){
     const img = document.createElement('img')
-    img.src = "https://dog.ceo/api/breeds/image/random/4"
-    document.getElementById('dog-breeds').append(img)
+    img.src = someUrl
+    document.getElementById('dog-image-container').append(img)
+}
+
+function fetchBreeds() {
+    fetch(breedUrl)
+    .then(response => response.json())
+    .then(dataDog => {
+        const breedArray = dataDog.message
+        breedArray.forEach(renderBreeds)
+    })
+}
+
+function renderBreeds(breedUrl){
+    const p = document.createElement('p')
+    p.innerText = breedUrl
+    document.getElementById('dog-breeds').append(p)
 }
